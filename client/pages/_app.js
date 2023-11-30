3e8fdca29f7e7c2c227ce205a781bf3a60c947d2
Aadil Saudagar <aadilsaudagar24@gmail.com>
2023-11-30 23:27:14 +0530

    Added Landing Page

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,29 +1,31 @@
-import Link from 'next/link'
-import '../styles/globals.css'
+import Link from "next/link";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return(
-      <div>
-        <nav className='border-b p-6'>
-          <p className='text-4xl font-bold'>Metaverse NFT shop</p>
-          <div className='flex mt-4'>
-            <Link href="/">
-              <a className='mr-4 text-pink-500'>Home</a>
-            </Link>
-            <Link href="/create-nft">
-              <a className='mr-6 text-pink-500'>Sell NFT</a>
-            </Link>
-            <Link href="/my-nfts">
-              <a className='mr-6 text-pink-500'>My Nft</a>
-            </Link>
-            <Link href="/dashboard">
-              <a className='mr-6 text-pink-500'>Dashboard</a>
-            </Link>
-          </div>
-        </nav>
-        <Component {...pageProps} />
-      </div>
-  )
+  return (
+    <div>
+      <nav className="border-b p-6">
+        <Link href="/landing2">
+          <a className="text-4xl font-bold">Metaverse NFT shop</a>
+        </Link>
+        <div className="flex mt-4">
+          <Link href="/">
+            <a className="mr-4 text-pink-500">Home</a>
+          </Link>
+          <Link href="/create-nft">
+            <a className="mr-6 text-pink-500">Sell NFT</a>
+          </Link>
+          <Link href="/my-nfts">
+            <a className="mr-6 text-pink-500">My Nft</a>
+          </Link>
+          <Link href="/dashboard">
+            <a className="mr-6 text-pink-500">Dashboard</a>
+          </Link>
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
